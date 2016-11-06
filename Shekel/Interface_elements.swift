e23a74d4e0a9b62bad933e08a253ecb471a16968
Evgeny Shmeerov <pine-trees@users.noreply.github.com>
2016-11-06 22:22:11 +0300

    random number while animation is playing

---
+++ Shekel/Interface_elements.swift
@@ -0,0 +1,21 @@
+//
+//  Interface_elements.swift
+//  Shekel
+//
+//  Created by Evgeny Shmeerov on 06/11/2016.
+//  Copyright © 2016 Eugene Shmeerov. All rights reserved.
+//
+
+import UIKit
+
+class Interface_elements: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
